8bb063a28a9afcbb856722046fa825cd193ed0aa
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-01-30 18:23:41 +0100

    Triangle Area

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,10 @@
 // scripts.js
 
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+//alert('Hello, ' + name);
 console.log('Hello, ' + name);
+/*
+  block comment
+  console.log('The commented will not be called');
+*/
+//line comment - this line does not exist
